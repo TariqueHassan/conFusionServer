e90fe6f294c1e55dd111fb6b03503af47d978cc9
TariqueHassan <tariquehassan.cs17@iba-suk.edu.pk>
2021-08-11 13:23:49 +0500

    Basic Authentication

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Promotions = require('../models/promotions');
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('Will send all the promotions to you!');
+    Promotions.find({})
+    .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req,res,next) => {
-    res.end('Will add all the promotions');
+    Promotions.create(req.body)
+    .then((promotion) => {
+        console.log('Promotion Created: ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported for /promotions');
 })
 .delete((req,res,next) => {
-    res.end('Will delete all the promotions');
+    Promotions.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-
 promoRouter.route('/:promoId')
 .get((req,res,next) => {
     res.end('Will send details of the promotion: ' + req.params.promoId);
